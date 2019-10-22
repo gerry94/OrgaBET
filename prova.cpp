8849207a3ab83fa3299774832a0f6ebb0871a0c4
riccardo <riccardo94p@hotmail.com>
2019-10-22 18:36:29 +0200

    ciao grr

--- prova.cpp
+++ prova.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 
+void stampa_shit()
+{
+	cout<<"pogrsttbruvaicàwRT4VQPOIZFEJÒSKOEMVOGD"<<endl;
+}
+
 int main()
 {
 	cout<<"ciao pollo."<<endl;
 	cout<<"come va?"<<endl;
-
+	
 	return 0;
 }
