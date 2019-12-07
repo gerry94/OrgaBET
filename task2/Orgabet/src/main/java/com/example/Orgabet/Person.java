9fd4bca30b1164e4f65e045904e730c0ef04c172
Giulio <giulio.silve92@gmail.com>
2019-12-07 14:02:09 +0100

    Backend user collection without coupon

--- task2/Orgabet/src/main/java/com/example/Orgabet/Person.java
+++ task2/Orgabet/src/main/java/com/example/Orgabet/Person.java
@@ -1,7 +1,10 @@
 package com.example.Orgabet;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
-
+@Getter
+@Setter
 public class Person {
 
   @Id private String id;
@@ -13,60 +16,4 @@ public class Person {
   private int privilege;
   private int banned;
 
-
-  public String getUserName() {
-    return userName;
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
- public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getFirstName() {
-    return lastName;
-  }	
- 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-  
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public int getPrivilege() {
-    return privilege;
-  }
-
-  public void setPrivilege(int privilege) {
-    this.privilege = privilege;
-  }
-
-  public int getBanned() {
-    return banned;
-  }
-
-  public void setBanned(int banned) {
-    this.banned = banned;
-  }
 }
