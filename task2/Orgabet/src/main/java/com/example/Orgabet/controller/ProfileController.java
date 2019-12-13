292bfe828c96e52fe26cb11601424697e46e90bc
Giulio <giulio.silve92@gmail.com>
2019-12-13 17:19:15 +0100

    Accesso a profili per user e admin (pagine da costruire e collegare)

--- task2/Orgabet/src/main/java/com/example/Orgabet/controller/ProfileController.java
+++ task2/Orgabet/src/main/java/com/example/Orgabet/controller/ProfileController.java
@@ -2,6 +2,8 @@ package com.example.Orgabet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.Orgabet.models.User;
 import com.example.Orgabet.repositories.UserRepository;
+import com.example.Orgabet.services.MongoUserDetailsService;
 
 @Controller
-@RequestMapping("/users")
-public class UsersController
+public class ProfileController
 {
-	
+	@Autowired
+	private MongoUserDetailsService userService;
 	@Autowired
 	private UserRepository repository;
 	
-    @GetMapping("/{username}")
-    @PreAuthorize("authentication.principal.username == #username) || hasRole('ADMIN')")
+    @GetMapping("admin/users/{username}")
     public String usersGet(@PathVariable("username") String username, Model model)
     {
     	User user=repository.findByUsername(username);
+    	model.addAttribute("user", user);
+        return "profile";
+    }
+    
+    @GetMapping("user/profile")
+    public String profileGet(Model model)
+    {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    User user = userService.findUserByUsername(auth.getName());
     	model.addAttribute("user", user);
         return "profile";
     }
